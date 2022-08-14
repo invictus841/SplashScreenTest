2d1bbe05613b5d8922ee47a04bfb70d3759ae796
NT <usedforfunny@gmail.com>
2022-08-14 20:32:45 +0200

    Initial Commit

---
+++ SplashScreenTest/SplashScreenTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashScreenTestApp.swift
+//  SplashScreenTest
+//
+//  Created by Alexandre Talatinian on 14/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SplashScreenTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
