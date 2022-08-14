7745bf41e0eba732674c3270d3812888c3dafed9
NT <usedforfunny@gmail.com>
2022-08-14 20:53:12 +0200

    Final Commit

--- SplashScreenTest/SplashScreenTestApp.swift
+++ SplashScreenTest/SplashScreenTestApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SplashScreenTestApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashScreenView()
         }
     }
 }
---
+++ SplashScreenTest/SplashScreenView.swift
@@ -0,0 +1,53 @@
+//
+//  SplashScreenView.swift
+//  SplashScreenTest
+//
+//  Created by Alexandre Talatinian on 14/08/2022.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State private var isActive = false
+    @State private var size = 0.8
+    @State private var opacity = 0.5
+    
+    var body: some View {
+        if isActive {
+            ContentView()
+        } else {
+            VStack {
+                       VStack {
+                           Image(systemName: "moon.stars.fill")
+                               .font(.system(size: 80))
+                               .foregroundColor(.indigo)
+                           Text("Luna")
+                               .font(Font.custom("Baskerville-Bold", size: 26))
+                               .foregroundColor(.black.opacity(0.80))
+                       }
+                       .scaleEffect(size)
+                       .opacity(opacity)
+                       .onAppear {
+                           withAnimation(.easeIn(duration: 1.2)) {
+                               self.size = 0.9
+                               self.opacity = 1.0
+                           }
+                       }
+                       
+                   }
+                   .onAppear {
+                       DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                           withAnimation {
+                               self.isActive = true
+                           }
+                       }
+                   }
+        }
+    }
+}
+
+struct SplashScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreenView()
+    }
+}
